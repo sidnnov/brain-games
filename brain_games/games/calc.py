26beff0a81d143e7dfac6b19a08b2d2ce0b5bb01
sidnnov <sidnnov@gmail.com>
2022-10-01 10:24:48 +0100

    Add game brain-calc(calc.py, brain_calc.py)

---
+++ brain_games/games/calc.py
@@ -0,0 +1,23 @@
+from brain_games.logics import try_again, welcome_user, comparison, question
+from brain_games.logics import congratulations, get_answer
+import random
+
+
+def calc():
+    name = welcome_user()
+    print('What is the result of the expression?')
+
+    for _ in range(3):
+        operation = random.choice(['-', '+', '*'])
+        num_1 = random.randint(1, 10)
+        num_2 = random.randint(1, 10)
+        answer = eval(str(num_1) + operation + str(num_2))
+        action = f'{num_1} {operation} {num_2}'
+        print(question(action))
+
+        if comparison(get_answer(), str(answer)):
+            return try_again(name)
+
+    return congratulations(name)
+
+# print(calc())
