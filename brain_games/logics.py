bd8da5f52f5014ff9d0186bc412ba14e442927bc
sidnnov <sidnnov@gmail.com>
2022-10-01 10:10:00 +0100

    Add logics.py

---
+++ brain_games/logics.py
@@ -0,0 +1,36 @@
+import prompt
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+
+def question(action):
+    return f'Question: {action}'
+
+
+def get_answer():
+    answer = prompt.string('Your answer: ')
+    return answer
+
+
+def comparison(answer_user, answer):
+    if answer_user == answer:
+        print('Correct!')
+        return False
+
+    else:
+        print(f'"{answer_user}" is wrong answer ;(. '
+              f'Correct answer was "{answer}".')
+        return True
+
+
+def try_again(name):
+    return f"Let's try again, {name}!"
+
+
+def congratulations(name):
+    return f'Congratulations, {name}!'
