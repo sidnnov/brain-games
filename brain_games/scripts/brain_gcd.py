ef36cec606a455fe8b110580978dbbb7a2bbfbf2
sidnnov <sidnnov@gmail.com>
2022-10-05 22:23:54 +0100

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.gcd import game_gcd
+from brain_games.games import gcd
+from brain_games.logic import start_game
 
 
 def main():
-    print(game_gcd())
+    print(start_game(gcd))
 
 
 if __name__ == '__main__':
