c99a9b0bc87680ce24c22055130f5c337c9e9e20
sidnnov <sidnnov@gmail.com>
2022-10-05 22:16:50 +0100

    Remove old func

--- brain_games/functions.py
+++ brain_games/functions.py
@@ -1,5 +1,4 @@
 from prompt import string
-from random import randint
 
 
 def welcome_user():
@@ -7,19 +6,6 @@ def welcome_user():
     name = string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
-
-
-def get_random_number(a, b):
-    return randint(a, b)
-
-
-def question(action):
-    return f'Question: {action}'
-
-
-def get_answer_user():
-    answer = string('Your answer: ')
-    return answer
 
 
 def is_comparison_answers(answer_user, answer):
@@ -31,11 +17,3 @@ def is_comparison_answers(answer_user, answer):
         print(f'"{answer_user}" is wrong answer ;(. '
               f'Correct answer was "{answer}".')
         return True
-
-
-def try_again(name):
-    return f"Let's try again, {name}!"
-
-
-def congratulations(name):
-    return f'Congratulations, {name}!'
