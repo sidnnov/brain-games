f3fec9304554389fac55900dabed5b95f32943cd
sidnnov <sidnnov@gmail.com>
2022-10-05 22:23:12 +0100

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.calc import game_calc
+from brain_games.games import calc
+from brain_games.logic import start_game
 
 
 def main():
-    print(game_calc())
+    print(start_game(calc))
 
 
 if __name__ == '__main__':
