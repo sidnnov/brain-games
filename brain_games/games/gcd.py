3b2ba72ea9833fb4c7bbf1c46504183c3f6d4453
sidnnov <sidnnov@gmail.com>
2022-10-06 08:53:57 +0100

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,12 +5,14 @@ def get_task_and_answer():
     num_1 = randint(1, 100)
     num_2 = randint(1, 100)
     task = f'{num_1} {num_2}'
+
     while num_1 != num_2:
         if num_1 > num_2:
             num_1 = num_1 - num_2
         else:
             num_2 = num_2 - num_1
     correct_answer = num_1
+
     return task, str(correct_answer)
 
 
