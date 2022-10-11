b0098171dc7b5c28dae1a7fa6a38162177530f4f
sidnnov <sidnnov@gmail.com>
2022-10-11 15:09:58 +0100

    Update parity_check.py

--- brain_games/games/parity_check.py
+++ brain_games/games/parity_check.py
@@ -1,11 +1,18 @@
 from random import randint
 
 
+MIN_NUMBER = 1
+MAX_NUMBER = 100
+RULES_OF_THE_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_evin(number):
+    return number % 2
+
+
 def get_task_and_answer():
-    task = randint(1, 100)
-    correct_answer = ['yes', 'no'][task % 2]
+    number = randint(MIN_NUMBER, MAX_NUMBER)
+    correct_answer = ['yes', 'no'][is_evin(number)]
+    task = number
 
     return task, correct_answer
-
-
-rules_of_the_game = 'Answer "yes" if the number is even, otherwise answer "no".'
