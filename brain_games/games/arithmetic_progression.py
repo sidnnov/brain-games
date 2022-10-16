af3678a0d39ef8442c2c01fb5358a9e9657fc78f
sidnnov <sidnnov@gmail.com>
2022-10-16 10:57:32 +0100

    Update arithmetic_progression.py

--- brain_games/games/arithmetic_progression.py
+++ brain_games/games/arithmetic_progression.py
@@ -11,16 +11,16 @@ MAX_INDEX_ANSWER = 9
 RULES_OF_THE_GAME = 'What number is missing in the progression?'
 
 
-def get_progression():
-    step = randint(MIN_STEP, MAX_STEP)
-    start = randint(MIN_START, MAX_START)
-    stop = start + (LENGTH * step)
+def get_progression(start, stop, step):
     progression = [str(i) for i in range(start, stop, step)]
     return progression
 
 
 def get_task_and_answer():
-    progression = get_progression()
+    step = randint(MIN_STEP, MAX_STEP)
+    start = randint(MIN_START, MAX_START)
+    stop = start + (LENGTH * step)
+    progression = get_progression(start, stop, step)
     index_correct_answer = randint(MIN_INDEX_ANSWER, MAX_INDEX_ANSWER)
     correct_answer = progression[index_correct_answer]
     progression[index_correct_answer] = '..'
