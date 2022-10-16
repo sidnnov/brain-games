a793542b216b652128c1f20ab78adc49f8c70338
sidnnov <sidnnov@gmail.com>
2022-10-16 11:00:57 +0100

    Renamed parity_check.py -> even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,13 +6,14 @@ MAX_NUMBER = 100
 RULES_OF_THE_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_evin(number):
+def is_even(number):
     return number % 2
 
 
 def get_task_and_answer():
     number = randint(MIN_NUMBER, MAX_NUMBER)
-    correct_answer = ['yes', 'no'][is_evin(number)]
+    result = is_even(number)
+    correct_answer = ['yes', 'no'][result]
     task = number
 
     return task, correct_answer
