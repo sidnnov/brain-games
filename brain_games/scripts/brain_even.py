b768bd2d298e8ae0dd2e37c7627f7b80374d6769
sidnnov <sidnnov@gmail.com>
2022-10-16 11:03:15 +0100

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games import parity_check
+from brain_games.games import even
 from brain_games.logic import start_game
 
 
 def main():
-    print(start_game(parity_check))
+    print(start_game(even))
 
 
 if __name__ == '__main__':
